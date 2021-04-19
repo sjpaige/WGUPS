172cfa92dce2d5691986c68d8e55d9fe90aee53c
sjpaige <sebastianpaige@gmail.com>
2021-04-19 16:54:41 -0700

    Changed to docstyle comments for DataStorage.
    Moved the images to new assets folder.

--- DataStorage.py
+++ DataStorage.py
@@ -1,21 +1,26 @@
-# DataStorage module connects the different types of data in the WGUPS program
-# and controls the level of response in the output
+"""
+The DataStorage module connects the different types of data in the WGUPS program and controls the level of response in
+the output.
+"""
 from StreetsGraph import StreetsGraph
 from DeliveryClock import DeliveryClock
 from HashTable import HashTable
 
-
-streets_map = StreetsGraph() # A undirected weighted graph data type for mapping the streets
-workday_clock = DeliveryClock("8:00 AM") # Custom clock
-packages = HashTable() # A self implremented hash table to store all the package data
-dev_response_enabled= False #Use to show more detailed output for the programs
+streets_map = StreetsGraph()  # A undirected weighted graph data type for mapping the streets
+workday_clock = DeliveryClock("8:00 AM")  # Custom clock
+packages = HashTable()  # A self implemented hash table to store all the package data
+dev_response_enabled = False  # Use to show more detailed output for the programs
 number_of_packages_in_data = 0
 
-# Searches the streets map and returns a specified location
-# this is a convienent interface with the streets_map locations
-# Time complexity of O(N) based on the number of locations in the input
+
 def search_map(name_or_address):
-    #search N items for the search term match
+    """
+    Searches the streets map and returns a specified location. This is a convenient interface with the streets_map
+    locations. Time complexity of O(N) based on the number of locations in the input.
+    :param name_or_address: a string search key
+    :return: The location that matches the search key
+    """
+    # search N items for the search term match
     for location in streets_map.adjacent_locations:
         if location.place_name == name_or_address:
             return location
@@ -23,16 +28,24 @@ def search_map(name_or_address):
             return location
     return None
 
-# Control of certain program outputs when creating WGUPS so that there output is not clogged with
-# lots of line by line print statements for all different functions.
+
 def dev_response(to_print):
+    """
+    Control of certain program outputs when creating WGUPS so that there output is not clogged with lots of line by
+    line print statements for all different functions.
+    :param to_print:
+    :return:
+    """
     if dev_response_enabled:
         print(to_print)
 
-# Clean up the data between different functions using it so that the program wont give out bad output
-# when dealing with the program running multiple outputs in a single session.
-# Time complexity of O(1)
+
 def clear_data():
+    """
+    Clean up the data between different functions using it so that the program wont give out bad output when dealing
+    with the program running multiple times in a single session.
+
+    Time complexity of O(1)
+    """
     streets_map.clear_data()
     packages.clear_data()
-    number_of_packages_in_data = 0
