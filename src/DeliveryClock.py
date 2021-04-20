2568cff5cbe2ab4d972eab13dbe9ccab0ecec875
sjpaige <sebastianpaige@gmail.com>
2021-04-20 12:06:54 -0700

    In DeliveryClock module, minor fixes to formatting, parse_string_time() made time_segments into list literal, rather than using append().

--- src/DeliveryClock.py
+++ src/DeliveryClock.py
@@ -1,5 +1,6 @@
 import math
 
+
 class DeliveryClock:
     """
     A class that creates clocks to track the time it takes the truck to make its deliveries.
@@ -8,7 +9,6 @@ class DeliveryClock:
     Built this way because a default python clock or time calculation was not simple enough or created specifically to
     interact with the travel_route() method.
     """
-
 
     def __init__(self, time_as_text):
         """
@@ -46,7 +46,6 @@ class DeliveryClock:
     #         print("afternoon_field_error: value must be AM or PM")
     #         exit()
 
-
     def compare_time(self, other_time):
         """
         Allows two clocks to be compared to one another.
@@ -64,15 +63,12 @@ class DeliveryClock:
         """
         return self.total_minutes_value - other_time.total_minutes_value
 
-
     def __str__(self):
         """
         Overrides the default method. Controls how the clock displays itself when printed out.
         Time complexity O(1)
         :return: the proper format for the clock as a string.
         """
-        hour_string = ""
-        minute_string = ""
         if self.hour > 12:
             hour_string = str(self.hour - 12)
         else:
@@ -85,7 +81,6 @@ class DeliveryClock:
 
         return hour_string + ":" + minute_string + " " + self.am_or_pm
 
-
     def add_time_minutes_only(self, minutes):
         """
         Adds a set amount of minutes to the clock and progresses time while tracking if its AM or PM designed to
@@ -97,7 +92,7 @@ class DeliveryClock:
         :param minutes: being added
         :return: the new time
         """
-        minutes = math.ceil(minutes) # get rid of pesky seconds upwards
+        minutes = math.ceil(minutes)  # get rid of pesky seconds upwards
         self.total_minutes_value += minutes
         self.hour = self.total_minutes_value // 60
         self.minute = self.total_minutes_value % 60
@@ -130,9 +125,6 @@ def parse_string_time(time_text="12:00 AM"):
     minute = temp.pop(0)
     am_or_pm = temp.pop(0)
 
-    time_segments = []
-    time_segments.append(hour)
-    time_segments.append(minute)
-    time_segments.append(am_or_pm)
+    time_segments = [hour, minute, am_or_pm]
 
     return time_segments
